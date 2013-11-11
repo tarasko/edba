5000cd94caf45d3b0f3952b99df293d9153f6b90
Taras Kozlov <kozlov.taras@gmail.com>
2013-11-11 08:32:04 +0400

    Fix doc deployment script

--- doc/build-and-upload.sh
+++ doc/build-and-upload.sh
@@ -1,17 +1,28 @@
+echo 'Invoke bjam two times'
+bjam
 bjam
 olddir=`pwd`
 
-tmpname=`mktemp.exe -d edba.XXXXXXXX`
+tmpname=`mktemp.exe -d --tmpdir edba.XXXXXXXX`
+echo "Use $tmpname as temporary folder"
+
 cd $tmpname
+
 git clone git@github.com:tarasko/edba.git
+cd edba
+
 git checkout --orphan gh-pages
-rm -rf .
+git rm -rf .
 rm '.gitignore'
 
-cp -R $olddir/html/* .
+cp -r $olddir/html/* .
+chmod -R u+rwx *
+
 git add .
 git commit -m 'Upload edba docs'
-git push origin gh-pages
+git push -f origin gh-pages
 
 cd $olddir
 rm -rf $tmpname
+
+exit 0
